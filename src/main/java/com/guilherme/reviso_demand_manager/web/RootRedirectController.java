8972ee204c9df6547829adabf68a432bb672f500
Guilherme <eleutherio.profissional@gmail.com>
2026-01-07 01:52:43 -0300

    feature(api): cutoff do legado e redirect do / para o frontend

---
+++ src/main/java/com/guilherme/reviso_demand_manager/web/RootRedirectController.java
@@ -0,0 +1,25 @@
+package com.guilherme.reviso_demand_manager.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootRedirectController {
+
+    private final String frontendBaseUrl;
+
+    public RootRedirectController(@Value("${frontend.base-url:http://localhost:4200}") String frontendBaseUrl) {
+        this.frontendBaseUrl = frontendBaseUrl;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Void> root() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, frontendBaseUrl)
+                .build();
+    }
+}
